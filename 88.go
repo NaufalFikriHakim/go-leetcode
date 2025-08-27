50c268c1eb1fa5c1236b935523f7b64b9e5ec995
Naufal Fikri Hakim <naufal.fikri11@ui.ac.id>
2025-08-27 09:43:58 +0700

    solved problem 88

--- 88.go
+++ 88.go
@@ -1,5 +1,12 @@
 package main
 
-func merge(nums1 []int, m int, nums2 []int, n int)  {
-    
+import "sort"
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+    index := 0
+    for index < n {
+        nums1[m+index] = nums2[index]
+        index++
+    } 
+	sort.Ints(nums1)
 }
